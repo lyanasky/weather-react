e188fd0425b99f8ff246d8375f4968e531bd33c5
lyana <137996973+lyanasky@users.noreply.github.com>
2023-07-29 17:46:02 -0700

    added new component

---
+++ src/Images/Image.js
@@ -0,0 +1,8 @@
+import Image from "./Images/BG.jpeg";
+import React from "react";
+
+export default function Image() {
+  <div className="Image">
+    <div style={{ backgroundImage: `url$image` }}></div>
+  </div>;
+}
