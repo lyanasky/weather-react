b55ff7f06c71b72ca1ac709316785bb5592f5ae1
lyana <137996973+lyanasky@users.noreply.github.com>
2023-07-31 13:07:02 -0700

    added background image

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import React from "react";
 import Weather from "./Weather.js";
 import "./App.css";
+const bgPicture = new URL("./images/BG.jpeg", import.meta.url);
 
 export default function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <Weather defaultCity="Abuja" />
-        <footer>
-          This project was coded by
-          <span className="coder">
-            <strong> Eliana Walters</strong> and is{" "}
-            <a
-              href="https://github.com/lyanasky/weather-react"
-              className="github-link"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Open-Sourced on GitHub
-            </a>
-            .
-          </span>
-        </footer>
+    <div className="main-container">
+      <img src={bgPicture} alt="BG" />
+      <div className="App">
+        <div className="container">
+          <Weather defaultCity="Abuja" />
+          <footer>
+            This project was coded by
+            <span className="coder">
+              <strong> Eliana Walters</strong> and is{" "}
+              <a
+                href="https://github.com/lyanasky/weather-react"
+                className="github-link"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Open-Sourced on GitHub
+              </a>
+              .
+            </span>
+          </footer>
+        </div>
       </div>
     </div>
   );
--- src/Images/Image.js
+++
@@ -1,8 +0,0 @@
-import Image from "./Images/BG.jpeg";
-import React from "react";
-
-export default function Image() {
-  <div className="Image">
-    <div style={{ backgroundImage: `url$image` }}></div>
-  </div>;
-}
