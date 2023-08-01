3903ae1bbc716d4f024a7e0b4b48278edee21542
lyana <137996973+lyanasky@users.noreply.github.com>
2023-08-01 01:21:06 -0700

    added background image

--- src/App.js
+++ src/App.js
@@ -6,31 +6,29 @@ import bgPicture from "./images/BG.jpeg";
 export default function App() {
   return (
     <div className="main-container">
-      <div className="background">
-        <img
-          src={bgPicture}
-          alt="BG"
-          style={{ position: "absolute", zIndex: -1 }}
-        />
-        <div className="App">
-          <div className="container">
-            <Weather defaultCity="Abuja" />
-            <footer>
-              This project was coded by
-              <span className="coder">
-                <strong> Eliana Walters</strong> and is{" "}
-                <a
-                  href="https://github.com/lyanasky/weather-react"
-                  className="github-link"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Open-Sourced on GitHub
-                </a>
-                .
-              </span>
-            </footer>
-          </div>
+      <img
+        src={bgPicture}
+        alt="BG"
+        style={{ position: "absolute", zIndex: -1 }}
+      />
+      <div className="App">
+        <div className="container">
+          <Weather defaultCity="Abuja" />
+          <footer>
+            This project was coded by
+            <span className="coder">
+              <strong> Eliana Walters</strong> and is{" "}
+              <a
+                href="https://github.com/lyanasky/weather-react"
+                className="github-link"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Open-Sourced on GitHub
+              </a>
+              .
+            </span>
+          </footer>
         </div>
       </div>
     </div>
