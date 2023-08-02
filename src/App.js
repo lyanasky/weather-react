2f97cdaf6d1ac13cbad73ad2c2defb36a359c22b
lyana <137996973+lyanasky@users.noreply.github.com>
2023-08-02 00:30:09 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,31 +6,32 @@ import bgPicture from "./Images/Background video.mp4";
 export default function App() {
   return (
     <div className="main-container">
-      <div className="backround">
-        <video
-          src={bgPicture}
-          alt="Backround video"
-          style={{ position: "absolute", zIndex: -1 }}
-        />
-        <div className="App">
-          <div className="container">
-            <Weather defaultCity="Abuja" />
-            <footer>
-              This project was coded by
-              <span className="coder">
-                <strong> Eliana Walters</strong> and is{" "}
-                <a
-                  href="https://github.com/lyanasky/weather-react"
-                  className="github-link"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Open-Sourced on GitHub
-                </a>
-                .
-              </span>
-            </footer>
-          </div>
+      <video
+        autoPlay
+        loop
+        muted
+        src={bgPicture}
+        alt="Background video"
+        style={{ position: "absolute", zIndex: -1 }}
+      />
+      <div className="App">
+        <div className="container">
+          <Weather defaultCity="Abuja" />
+          <footer>
+            This project was coded by
+            <span className="coder">
+              <strong> Eliana Walters</strong> and is{" "}
+              <a
+                href="https://github.com/lyanasky/weather-react"
+                className="github-link"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Open-Sourced on GitHub
+              </a>
+              .
+            </span>
+          </footer>
         </div>
       </div>
     </div>
